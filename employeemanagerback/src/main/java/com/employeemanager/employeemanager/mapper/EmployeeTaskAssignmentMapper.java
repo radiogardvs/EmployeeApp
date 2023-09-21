f853b622ec64abf14699325e869ab47bbb400fc1
radiogardvs <radiogardvs@gmail.com>
2023-09-21 21:57:16 +0300

    .

--- employeemanagerback/src/main/java/com/employeemanager/employeemanager/mapper/EmployeeTaskAssignmentMapper.java
+++ employeemanagerback/src/main/java/com/employeemanager/employeemanager/mapper/EmployeeTaskAssignmentMapper.java
@@ -1,26 +1,43 @@
 package com.employeemanager.employeemanager.mapper;
 import com.employeemanager.employeemanager.dao.EmployeeTaskAssignment;
 import com.employeemanager.employeemanager.dto.EmployeeTaskAssignmentDTO;
+import com.employeemanager.employeemanager.repository.EmployeeRepository;
 import com.employeemanager.employeemanager.repository.EmployeeTaskAssignmentRepository;
+import com.employeemanager.employeemanager.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmployeeTaskAssignmentMapper {
     private final EmployeeTaskAssignmentRepository employeeTaskAssignmentRepository;
+    private final EmployeeRepository employeeRepository;
+    private final TaskRepository taskRepository;
     @Autowired
-    public EmployeeTaskAssignmentMapper(EmployeeTaskAssignmentRepository employeeTaskAssignmentRepository){
+    public EmployeeTaskAssignmentMapper(EmployeeTaskAssignmentRepository employeeTaskAssignmentRepository, EmployeeRepository employeeRepository, TaskRepository taskRepository){
         this.employeeTaskAssignmentRepository=employeeTaskAssignmentRepository;
+        this.employeeRepository=employeeRepository;
+        this.taskRepository=taskRepository;
     }
-    public EmployeeTaskAssignmentDTO toDTO(EmployeeTaskAssignment assignment) {
-        EmployeeTaskAssignmentDTO assignmentDTO = new EmployeeTaskAssignmentDTO();
-        assignmentDTO.setId(assignment.getId());
-        assignmentDTO.setEmployeeId(assignment.getEmployee().getId());
-        assignmentDTO.setTaskId(assignment.getTask().getId());
-        return assignmentDTO;
+    public EmployeeTaskAssignmentDTO toDTO(EmployeeTaskAssignment employeeTaskAssignment){
+        EmployeeTaskAssignmentDTO employeeTaskAssignmentDTO= new EmployeeTaskAssignmentDTO();
+        employeeTaskAssignmentDTO.setId(employeeTaskAssignment.getId());
+        employeeTaskAssignmentDTO.setEmployeeId(employeeTaskAssignment.getEmployee().getId());
+        employeeTaskAssignmentDTO.setTaskId(employeeTaskAssignment.getTask().getId());
+        return employeeTaskAssignmentDTO;
     }
-
-    public EmployeeTaskAssignment toEntity(EmployeeTaskAssignmentDTO assignmentDTO) {
-        return employeeTaskAssignmentRepository.findById(assignmentDTO.getId()).orElseThrow();
+    public EmployeeTaskAssignment findEntity(EmployeeTaskAssignmentDTO employeeTaskAssignmentDTO){
+        return employeeTaskAssignmentRepository.findById(employeeTaskAssignmentDTO.getId()).orElseThrow();
+    }
+    public EmployeeTaskAssignment toEntity(EmployeeTaskAssignmentDTO employeeTaskAssignmentDTO){
+        EmployeeTaskAssignment employeeTaskAssignment=new EmployeeTaskAssignment();
+        employeeTaskAssignment.setEmployee(employeeRepository.findById(employeeTaskAssignmentDTO.getEmployeeId()).orElseThrow());
+        employeeTaskAssignment.setTask(taskRepository.findById(employeeTaskAssignmentDTO.getTaskId()).orElseThrow());
+        return employeeTaskAssignment;
+    }
+    public EmployeeTaskAssignment updateEntity(EmployeeTaskAssignmentDTO employeeTaskAssignmentDTO){
+        EmployeeTaskAssignment employeeTaskAssignment= employeeTaskAssignmentRepository.findById(employeeTaskAssignmentDTO.getId()).orElseThrow();
+        employeeTaskAssignment.setEmployee(employeeRepository.findById(employeeTaskAssignmentDTO.getEmployeeId()).orElseThrow());
+        employeeTaskAssignment.setTask(taskRepository.findById(employeeTaskAssignmentDTO.getTaskId()).orElseThrow());
+        return employeeTaskAssignment;
     }
 }
--- employeemanagerback/src/main/java/com/employeemanager/employeemanager/service/EmployeeService.java
+++ employeemanagerback/src/main/java/com/employeemanager/employeemanager/service/EmployeeService.java
@@ -1,27 +1,27 @@
 package com.employeemanager.employeemanager.service;
-
-import com.employeemanager.employeemanager.dao.Task;
 import com.employeemanager.employeemanager.dto.EmployeeDTO;
-import com.employeemanager.employeemanager.exception.EmployeeNotFoundException;
-import com.employeemanager.employeemanager.dao.Employee;
+import com.employeemanager.employeemanager.dto.EmployeeTaskAssignmentDTO;
 import com.employeemanager.employeemanager.mapper.EmployeeMapper;
+import com.employeemanager.employeemanager.mapper.EmployeeTaskAssignmentMapper;
 import com.employeemanager.employeemanager.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
-
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper employeeMapper;
+    private final EmployeeTaskAssignmentMapper employeeTaskAssignmentMapper;
+    private final EmployeeTaskAssignmentRepository employeeTaskAssignmentRepository;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository, EmployeeMapper employeeMapper) {
+    public EmployeeService(EmployeeRepository employeeRepository, EmployeeMapper employeeMapper, EmployeeTaskAssignmentMapper employeeTaskAssignmentMapper,
+                           EmployeeTaskAssignmentRepository employeeTaskAssignmentRepository) {
         this.employeeRepository = employeeRepository;
-        this.employeeMapper= employeeMapper;
+        this.employeeMapper = employeeMapper;
+        this.employeeTaskAssignmentMapper = employeeTaskAssignmentMapper;
+        this.employeeTaskAssignmentRepository=employeeTaskAssignmentRepository;
     }
 
     public EmployeeDTO addEmployee(EmployeeDTO employeeDTO) {
@@ -44,5 +44,8 @@ public class EmployeeService {
         employeeRepository.deleteById(id);
     }
 
+    public EmployeeTaskAssignmentDTO assignTask(EmployeeTaskAssignmentDTO employeeTaskAssignmentDTO){
+        return employeeTaskAssignmentMapper.toDTO(employeeTaskAssignmentRepository.save(employeeTaskAssignmentMapper.toEntity(employeeTaskAssignmentDTO)));
+    }
 
 }
